a83cdb0efa87dec61a1847555511b7874d992363
Hafsa Ahmad <hafsa.ahmad@kcl.ac.uk>
2022-10-18 20:29:48 +0100

    complete prime function to give a list of primes of length n

--- primes.py
+++ primes.py
@@ -1,6 +1,16 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+prime30 =[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
 def primes(number_of_primes):
-    list = []
-    return list
+    list1 = []
+    list1 = prime30[:number_of_primes]
+
+    print(list1)
+    return list1
+
+
+number = int(input("enter a positive number > 0: "))
+
+primes(number)
